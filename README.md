077dfeeda3358e812438ca989d88542a799142f7
Jordan Knight <jordan_r_knight@hotmail.com>
2017-03-17 19:37:58 +0900

    Transfered project to GitHub repository
    Completed tutorial project.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Roll-a-ball
-Unity tutorial rolling ball game.
+Unity tutorial rolling ball game
+
+## About 
+
+This project was created with Unity for educational purposes.
+
+## Credits
+
+Created by Jordan Knight.
+
+### Tutorial
+
+https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+    int count;
+    public float speed;
+    public Text countText, winText;
+
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    void Start()
+    {
+        SetCountText();
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerticle = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVerticle);
+        rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString();
+        if(count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
